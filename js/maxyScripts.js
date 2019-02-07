452a748e03773f4164b10350925cbd9742d6c54f
Jordan <jordantmarshall3@gmail.com>
2019-02-07 10:17:56 -0600

    added client side breadcrumb trail func

--- js/maxyScripts.js
+++ js/maxyScripts.js
@@ -226,3 +226,45 @@ if (window.location.host != "www.life-healing.com" && window.location.host != "w
         });
     });
 }
+// END Video plays action
+
+// START Breadcrumb trail for webform analytics
+(function () {
+    // Set cookie with name(key) and value
+    function setCookie(name, value) {
+      document.cookie = name + "=" + value;
+    }
+  
+    // Retrieve cookie value by name(key)
+    function getCookie(name) {
+      var reg = new RegExp(name + "=([^;]+)");
+      var val = reg.exec(document.cookie);
+      // Return parsed JSON if not null
+      return (val != null) ? val[1] : null;
+    }
+  
+    // Get current page
+    var currentPath = document.location.pathname;
+    // Get last page from referrer; replacing host to just get path
+    var lastPath = document.referrer.replace(location.origin, '');
+  
+    // If not same page as last and navigation.type was not a refresh
+    if (currentPath !== lastPath && performance.navigation.type === 0) {
+      var cookieName = 'breadcrumbs';
+      // Use session storage to read and update cookie; because it is more consistent
+      var seshCookie = sessionStorage.getItem(cookieName);
+  
+      if (seshCookie === null) {
+        sessionStorage.setItem(cookieName, currentPath);
+      } else {
+        // If cookie not empty append current path to original value
+        seshCookie += '>' + currentPath;
+        console.log('update cookie: ', seshCookie);
+        sessionStorage.setItem(cookieName, seshCookie);
+      }
+  
+      // Overwrite cookie with session storage val
+      setCookie(cookieName, seshCookie);
+    }
+  })();
+  // END Breadcrumbs
