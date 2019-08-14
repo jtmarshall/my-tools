88c11b88fbcaf972d2c8eae926ab4da3918505ec
Jordan <jordan.marshall@acadiahealthcare.com>
2019-08-14 10:16:27 -0500

    add phonescrape

---
+++ go/phoneScrape/build.sh
@@ -0,0 +1,5 @@
+go get github.com/go-sql-driver/mysql
+go get github.com/jasonlvhit/gocron
+go get golang.org/x/net/html
+go get gopkg.in/robfig/cron.v2
+go build -o bin/application application.go
---
+++ go/phoneScrape/templates/build/precache-manifest.3f5d8895a70f36d9f95fa7b2417eb034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6e0e09b4d8d72c2ae304c6450a21a09",
+    "url": "/static/media/yak-logo-fullbody.a6e0e09b.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1dbb195264677cf78304",
+    "url": "/static/js/main.1dbb1952.chunk.js"
+  },
+  {
+    "revision": "3db6a8257000def5d0c2",
+    "url": "/static/js/1.3db6a825.chunk.js"
+  },
+  {
+    "revision": "1dbb195264677cf78304",
+    "url": "/static/css/main.ec4ca6ff.chunk.css"
+  },
+  {
+    "revision": "3db6a8257000def5d0c2",
+    "url": "/static/css/1.cfe51acb.chunk.css"
+  },
+  {
+    "revision": "c67d99173a510c35f5c05184fa740310",
+    "url": "/index.html"
+  }
+];
